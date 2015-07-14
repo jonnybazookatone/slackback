37fad4bc4f31ff66c22d03a0ba2cd398ffe0c989
Jonny Elliott <jonnynelliott@googlemail.com>
2015-07-13 20:32:11 -0400

    Removed syslog for travisci

--- slackback/config.py
+++ slackback/config.py
@@ -36,17 +36,11 @@ SAMPLE_APPLICATION_LOGGING = {
             'formatter': 'default',
             'level': 'DEBUG',
             'class': 'logging.StreamHandler'
-        },
-        'syslog': {
-            'formatter': 'default',
-            'level': 'DEBUG',
-            'class': 'logging.handlers.SysLogHandler',
-            'address': '/dev/log'
         }
     },
     'loggers': {
         '': {
-            'handlers': ['file', 'console', 'syslog'],
+            'handlers': ['file', 'console'],
             'level': 'CRITICAL',
             'propagate': True,
         },
